5f1872ac6cc303a8bc0392c45bca25eb9fd864e8
Mohammad Murad <2023nitsgr280@nitsri.ac.in>
2025-12-24 11:20:02 +0530

    Initial commit

---
+++ server.py
@@ -0,0 +1,73 @@
+import socket
+import pickle
+from colorama import Fore, Style, init
+
+init(autoreset=True)
+
+HOST = "10.25.197.61"
+PORT = 5050
+
+board = [" "] * 9
+
+def colorize(cell):
+    if cell == "X":
+        return Fore.RED + "X" + Style.RESET_ALL
+    elif cell == "O":
+        return Fore.BLUE + "O" + Style.RESET_ALL
+    return " "
+
+def print_board(b):
+    print(Fore.YELLOW + "\n   TIC TAC TOE (You = X)\n")
+    print(f"   {colorize(b[0])} | {colorize(b[1])} | {colorize(b[2])}")
+    print("  ---+---+---")
+    print(f"   {colorize(b[3])} | {colorize(b[4])} | {colorize(b[5])}")
+    print("  ---+---+---")
+    print(f"   {colorize(b[6])} | {colorize(b[7])} | {colorize(b[8])}\n")
+
+def check_winner(b):
+    wins = [(0,1,2),(3,4,5),(6,7,8),
+            (0,3,6),(1,4,7),(2,5,8),
+            (0,4,8),(2,4,6)]
+    for a,c,d in wins:
+        if b[a] == b[c] == b[d] != " ":
+            return b[a]
+    if " " not in b:
+        return "Draw"
+    return None
+
+sock = socket.socket()
+sock.bind((HOST, PORT))
+sock.listen(1)
+print(Fore.GREEN + "[SERVER] Waiting for player O...")
+
+c, addr = sock.accept()
+print(Fore.CYAN + "[SERVER] Player connected:", addr)
+
+while True:
+    print_board(board)
+
+    try:
+        move = int(input(Fore.GREEN + "Your move (0-8): "))
+    except:
+        continue
+
+    if move < 0 or move > 8 or board[move] != " ":
+        print(Fore.RED + "Invalid move!")
+        continue
+
+    board[move] = "X"
+    winner = check_winner(board)
+
+    c.send(pickle.dumps((board, winner)))
+
+    if winner:
+        print_board(board)
+        print(Fore.MAGENTA + "Result:", winner)
+        break
+
+    board, winner = pickle.loads(c.recv(1024))
+
+    if winner:
+        print_board(board)
+        print(Fore.MAGENTA + "Result:", winner)
+        break
